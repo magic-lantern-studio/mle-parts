8e7bc1a35afe484f1830cf1e0ffd68b53fe8f5e1
Mark Millard <msm@wizzerworks.com>
2020-02-11 13:22:12 -0700

    Removing obsolete build environment.

--- Parts/roles/inventor/build/linux/include/ivroles.h
+++
@@ -1,8 +0,0 @@
-#ifndef ivroles_H
-#define ivroles_H
-
-#include "stdio.h"
-
-extern void print_hello ();
-
-#endif
--- Parts/roles/inventor/build/linux/ivroles/ivroles.c
+++
@@ -1,6 +0,0 @@
-#include "ivroles.h"
-
-void
-print_hello(){
-  printf("Hello World\n");
-}
